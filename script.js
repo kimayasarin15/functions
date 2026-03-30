8db19149e1681c770cf6f6f0ce49afa7c797c6c8
kimayasarin15 <sarik831@newschool.edu>
2026-03-30 16:14:32 -0400

    adding form + canvas

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+// I added my image at the base of the canvas
+const img = new Image();
+
+img.addEventListener("load", () => {
+  ctx.drawImage(img, 0, 0);
+});
+
+img.src = "postertest.png"; 
+
