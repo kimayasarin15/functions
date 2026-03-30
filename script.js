15b67a7efef0a052de701ed345239d6cef011c6b
kimayasarin15 <sarik831@newschool.edu>
2026-03-30 16:28:39 -0400

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
-// I added my image at the base of the canvas
+// I added my image at the base of the canvas 
 const img = new Image();
 
 img.addEventListener("load", () => {
@@ -10,4 +10,7 @@ img.addEventListener("load", () => {
 });
 
 img.src = "postertest.png"; 
+
+
+
 
