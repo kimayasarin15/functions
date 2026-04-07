45f76e8f6867ea14bbd5658d1eb3fa91fe2903b0
kimayasarin15 <sarik831@newschool.edu>
2026-04-07 16:57:40 -0400

    added classes for different mockups + fixed image issue

--- script.js
+++ script.js
@@ -33,26 +33,34 @@ input2.addEventListener('input', () => {
 
 const style1 = document.querySelector('#style-1');
 style1.addEventListener('click', () => {
-    preview.classList.remove('style-1', 'style-2', 'style-3');
+    preview.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
     preview.classList.add('style-1');
-    preview2.classList.remove('style-1', 'style-2', 'style-3');
+    preview2.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
     preview2.classList.add('style-1');
 });
 
 const style2 = document.querySelector('#style-2');
 style2.addEventListener('click', () => {
-    preview.classList.remove('style-1', 'style-2', 'style-3');
+    preview.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
     preview.classList.add('style-2');
-    preview2.classList.remove('style-1', 'style-2', 'style-3');
+    preview2.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
     preview2.classList.add('style-2');
 });
 
 const style3 = document.querySelector('#style-3');
 style3.addEventListener('click', () => {
-    preview.classList.remove('style-1', 'style-2', 'style-3');
+    preview.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
     preview.classList.add('style-3');
-    preview2.classList.remove('style-1', 'style-2', 'style-3');
+    preview2.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
     preview2.classList.add('style-3');
+});
+
+const style4 = document.querySelector('#style-4');
+style4.addEventListener('click', () => {
+    preview.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
+    preview.classList.add('style-4');
+    preview2.classList.remove('style-1', 'style-2', 'style-3', 'style-4');
+    preview2.classList.add('style-4');
 });
 
 // adding my color pickers https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/color
@@ -89,6 +97,8 @@ input3.addEventListener('change', () => {
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/src - changing the image src with JS and a button
+
+// https://claude.ai/share/679d9a71-65bc-4683-9a79-91a3ab7643eb I pasted in the same question to Claude and asked for it not to give me any code - I just wanted to check this was the correct way to do it - I'm adding and removing my classes then I click the buttons
 const image = document.querySelector('#image');
 const image1 = document.querySelector('#image-1');
 const image2 = document.querySelector('#image-2');
@@ -96,12 +106,24 @@ const image3 = document.querySelector('#image-3');
 
 image1.addEventListener('click', () => {
     image.src = 'subway.png';
+    preview.classList.remove('mainlayout-billboard', 'mainlayout-sidewalk');
+    preview.classList.add('mainlayout-subway');
+    preview2.classList.remove('secondlayout-billboard', 'secondlayout-sidewalk');
+    preview2.classList.add('secondlayout-subway');
 });
 
 image2.addEventListener('click', () => {
     image.src = 'billboard.png';
+    preview.classList.remove('mainlayout-subway', 'mainlayout-sidewalk');
+    preview.classList.add('mainlayout-billboard');
+    preview2.classList.remove('secondlayout-subway', 'secondlayout-sidewalk');
+    preview2.classList.add('secondlayout-billboard');
 });
 
 image3.addEventListener('click', () => {
     image.src = 'sidewalk.png';
+    preview.classList.remove('mainlayout-subway', 'mainlayout-billboard');
+    preview.classList.add('mainlayout-sidewalk');
+    preview2.classList.remove('secondlayout-subway', 'secondlayout-billboard');
+    preview2.classList.add('secondlayout-sidewalk');
 });
